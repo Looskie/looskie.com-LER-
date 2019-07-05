b84decb2cd905078db9bbe12f56b081637a02890
looskie <50378828+Looskie@users.noreply.github.com>
2019-07-05 11:13:03 -0400

    Add files via upload

---
+++ LERAssets/Information/assets/prices/js/main.js
@@ -0,0 +1,38 @@
+(function($) { "use strict";
+        var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+})(jQuery); 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	setTimeout(() => {
+		document.body.classList.add('loaded');
+	}, 1100)
+});
+
+document.querySelector('.projects-link').addEventListener('click', () => {
+	document.body.classList.remove('loaded');
+	document.body.classList.add('hidding');
+    setTimeout(function() {
+    window.location.assign("https://looskie.com/LERAssets/Information/rooms");
+    }, 1900);
+});
