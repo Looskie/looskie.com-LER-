132296826e35137ccf374c91d01c00b0c331c080
looskie <50378828+Looskie@users.noreply.github.com>
2019-07-05 11:10:32 -0400

    Create main1.js

---
+++ LERAssets/Information/assets/js/main1.js
@@ -0,0 +1,44 @@
+(function($) { "use strict";
+        var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+})(jQuery); 
+document.body.addEventListener('dblclick', () => {
+    document.body.classList.remove('loaded');
+    document.body.classList.add('hidding');
+    setTimeout(function() {
+        window.location.assign("https://www.Looskie.com/LERAssets/Information/prices");
+    }, 1900);
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	setTimeout(() => {
+		document.body.classList.add('loaded');
+	}, 100)
+});
+
+document.querySelector('.projects-link').addEventListener('click', () => {
+	document.body.classList.remove('loaded');
+	document.body.classList.add('hidding');
+    setTimeout(function() {
+    window.location.assign("https://looskie.com/LERAssets/Information/prices");
+    }, 1900);
+});
